e011886e521848a753974ffcaac2c115fa18dc49
staefy <piledahl@gmail.com>
2014-12-31 21:08:12 +0100

    Added button

--- energy.js
+++ energy.js
@@ -11,22 +11,15 @@
 
         var self = this;
 
-        self.energyExpendatures = [
+        self.allEnergy = [
             {name: '1km Bike ride', energy: 146400, type:'expendature'},
             {name: '1km Car drive', energy: 2.72e6, type:'expendature'},
             {name: '1km walking', energy: 0.22e6, type:'expendature'},
-            {name: 'Impact by air gun', energy: 10, type:'kinetic'},
-            {name: 'Impact by hunting rifle', energy: 1000, type:'kinetic'},
-            {name: 'Human punch', energy: 150, type:'expendature'},
             {name: '1day of human life', energy: 10e6, type:'expendature'},
             {name: '10min hot shower', energy: 20e6, type:'expendature'},
             {name: '1sec Earths solar absorption', energy: 0.1221e18, type:'production'},
             {name: '1sec World energy consumption', energy: 1.6288038e13, type:'expendature'},
-            {name: '1hr average wind turbine', energy: 2.46e9, type:'production'}
-
-        ];
-
-        self.energyStores = [
+            {name: '1hr average wind turbine', energy: 2.46e9, type:'production'},
             {name: '1litre Gasoline', energy: 33.92e6, type:'store'},
             {name: '1kg wood for burning', energy: 10e6, type:'store'},
             {name: '1litre Water at 10m', energy: 0.0001e6, type:'store'},
@@ -67,7 +60,6 @@
          * Bullet
          */
 
-        self.allEnergy = self.energyExpendatures.concat(self.energyStores);
     }
 
     /**
@@ -78,11 +70,7 @@
     function EnergyCtrl($scope, EnergyService){
         $scope.EnergyService = EnergyService;
 
-
-
-
         // after we've selected a thing, we need to trigger the recalculation
-
         $scope.recalc = function(){
 
             var comparable = $scope.selectedCompare;
@@ -93,12 +81,50 @@
             // Take the first thing. compare it to the other thing
             // How many bananas are there in a mars bar?
             if(ratio < 1){
-                ratio = ratio * 100 +'%';
+                ratio = ratio * 100 +'% of';
             }else{
                 ratio += 'x'
             }
             $scope.ratio = ratio;
         }
+
+        $scope.loadRandom = function(){
+
+            //TODO Fetch from a curated pool
+            var randomNumber1 = Math.floor((Math.random() * EnergyService.allEnergy.length));
+            var randomNumber2 = Math.floor((Math.random() * EnergyService.allEnergy.length));
+            var pick1 = EnergyService.allEnergy[randomNumber1];
+            var pick2 = EnergyService.allEnergy[randomNumber2];
+            //Prevent same-same comparison
+            if(pick1 == pick2){
+                randomNumber2 = Math.floor((Math.random() * EnergyService.allEnergy.length));
+                pick2 = EnergyService.allEnergy[randomNumber2];
+            }
+            //Populate view
+            $scope.selectedComparee = pick1; 
+            $scope.selectedCompare = pick2;
+            $scope.recalc();
+        }
+
+        $scope.switchPlaces = function(){
+
+            //TODO Fetch from a curated pool
+            var randomNumber1 = Math.floor((Math.random() * EnergyService.allEnergy.length));
+            var randomNumber2 = Math.floor((Math.random() * EnergyService.allEnergy.length));
+            var pick1 = EnergyService.allEnergy[randomNumber1];
+            var pick2 = EnergyService.allEnergy[randomNumber2];
+            //Prevent same-same comparison
+            if(pick1 == pick2){
+                randomNumber2 = Math.floor((Math.random() * EnergyService.allEnergy.length));
+                pick2 = EnergyService.allEnergy[randomNumber2];
+            }
+            //Populate view
+            $scope.selectedComparee = pick1; 
+            $scope.selectedCompare = pick2;
+            $scope.recalc();
+        }
+        //Start out with random entries
+        $scope.loadRandom();
     }
 
 })();
