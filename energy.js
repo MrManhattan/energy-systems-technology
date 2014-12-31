146268a0462839a214115727caa209c59c32144f
staefy <piledahl@gmail.com>
2014-12-31 17:55:19 +0100

    More stuff

--- energy.js
+++ energy.js
@@ -14,20 +14,22 @@
         self.energyExpendatures = [
             {name: '1km Bike ride', energy: 146400, type:'exp'},
             {name: '1km Car drive', energy: 2.72e6, type:'exp'},
-            {name: 'Shot by air gun', energy: 10, type:'kinetic'},
-            {name: 'Shot by hunting rifle', energy: 1000, type:'kinetic'},
+            {name: 'Impact by air gun', energy: 10, type:'kinetic'},
+            {name: 'Impact by hunting rifle', energy: 1000, type:'kinetic'},
             {name: 'Human punch', energy: 150, type:'exp'},
             {name: '1day of human life', energy: 10e6, type:'exp'},
             {name: '10min hot shower', energy: 20e6, type:'exp'},
-            {name: 'World energy consumption by second', energy: 1.6288038e13}
+            {name: '1sec Earths solar absorption', energy: 0.1221e18, type:'exp'},
+            {name: '1sec World energy consumption', energy: 1.6288038e13}
 
         ];
 
         self.energyStores = [
-            {name: 'Litre gasoline', energy: 33.92e6, type:'store'},
-            {name: '1litre water at 10m', energy: 0.0001e6, type:'store'},
-            {name: '1kg compressed air', energy: 0.5e6, type:'store'},
-            {name: 'Anti matter', energy: 1.8e11, type:'store'},
+            {name: '1litre Gasoline', energy: 33.92e6, type:'store'},
+            {name: '1kg wood for burning', energy: 10e6, type:'store'},
+            {name: '1litre Water at 10m', energy: 0.0001e6, type:'store'},
+            {name: '1kg Compressed air', energy: 0.5e6, type:'store'},
+            {name: '1kg Anti matter', energy: 1.8e11, type:'store'},
             {name: '1kg Human Fat', energy: 37e6, type:'store'},
             {name: 'AA Battery', energy: 0.0154e6, type:'store'},
             {name: 'Car Battery', energy: 2.6e6, type:'store'},
